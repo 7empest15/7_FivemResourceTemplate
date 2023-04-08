ae9bfffa84990e047709d2ac50b7122873d65f3a
7empest15 <tristanjacob015@gmail.com>
2023-04-08 20:50:18 +0200

    First commit

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author '7empest15'
+client_scripts {
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/*.lua',
+}
+
+shared_scripts {
+    'config.lua',
+    'shared/*.lua',
+}
+
+lua54 'yes'
